91ea0e065f76ea08e0061a8825fc340d8d57c280
Major-Eth <major-tom.eth@pm.me>
2022-01-31 16:57:27 +0100

    🚧 WIP

---
+++ contracts/yearnMetalVault.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+interface Vault is IERC20 {
+    function decimals() external view returns (uint256);
+
+    function deposit() external returns (uint256);
+
+    function deposit(uint256 amount) external returns (uint256);
+
+    function deposit(uint256 amount, address recipient)
+        external
+        returns (uint256);
+
+    function withdraw() external returns (uint256);
+
+    function withdraw(uint256 maxShares) external returns (uint256);
+
+    function withdraw(uint256 maxShares, address recipient)
+        external
+        returns (uint256);
+
+    function token() external view returns (address);
+
+    function pricePerShare() external view returns (uint256);
+
+    function totalAssets() external view returns (uint256);
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 amount,
+        uint256 expiry,
+        bytes calldata signature
+    ) external returns (bool);
+}
+
+interface Bowswap {
+    enum Action {Deposit, Withdraw, Swap}
+    struct Swap {
+        Action action;
+        address pool;
+        uint128 n;
+        uint128 m;
+    }
+    function swap(address from_vault, address to_vault, uint256 amount, uint256 min_amount_out, Swap[] calldata instructions, uint256 donation, uint256 origin) external;
+    function estimate_out(address from_vault, address to_vault, uint256 amount, Swap[] calldata instructions, uint256 donation) external view returns (uint256);
+}
+
+interface   YearnRegistry {
+    function latestVault(address) external view returns (address);
+}
+
+contract MetaVault {
+    YearnRegistry public constant yRegistry = YearnRegistry(0x50c1a2eA0a861A967D9d0FFE2AE4012c2E053804);
+    Bowswap public bowswap = Bowswap(0xaF0CB47a1bB9115DCA563BcC7C2EAbD711cCa685);
+    Vault public currentVault;
+    uint256 public currentDecimals;
+    uint256 public power = 1e18;
+    uint256 public totalShares;
+    mapping(address => uint256) public shares;
+    mapping(address => mapping(address => uint256)) public balanceOfIn;
+
+    string constant public name = "MetaVault curveEURS + curveEURT + curveibEUR + curveEURN + curve3EUR";
+
+    constructor(address _token, address _bowswap) {
+        currentVault = Vault(yRegistry.latestVault(_token));
+        currentDecimals = currentVault.decimals();
+        bowswap = Bowswap(_bowswap);
+    }
+
+
+    /**
+        @notice Retrieve, for a given _owner, the number of shares it owns in this metavault
+        @param _owner Address for which we want the number of shares
+        @return Shares in WEI
+    */
+    function getShares(address _owner) public view returns (uint256) {
+        return shares[_owner] * power / 1e18;
+    }
+
+    /**
+        @notice Helper function to retrieve the total number of shares for
+        the whole metavault. Match the balanceOf this in the currentVault.
+        @return balanceOf this in currentVault
+    */
+    function metaVaultTotalShare() public view returns (uint256) {
+        return currentVault.balanceOf(address(this));
+    }
+
+    /**
+        @notice Helper function to retrieve the currentVault pricePerShare.
+        @return Price per share of the currentVault
+    */
+    function underlyingPricePerShare() public view returns (uint256) {
+        return currentVault.pricePerShare();
+    }
+
+	function	deposit(uint256 _amountToDeposit, address _recipient) public returns (uint256) {
+		IERC20 _currentVaultWant = IERC20(currentVault.token());
+		uint256 _depositorBalance = _currentVaultWant.balanceOf(msg.sender);
+		if (_depositorBalance < _amountToDeposit) {
+			_amountToDeposit = _depositorBalance;
+		}
+		_currentVaultWant.transferFrom(msg.sender, address(this), _amountToDeposit);
+
+        _currentVaultWant.approve(address(currentVault), _amountToDeposit);
+		uint256 _shareForDepositor = currentVault.deposit(_amountToDeposit);
+
+		shares[_recipient] += _shareForDepositor;
+        totalShares = totalShares + _shareForDepositor;
+		return _shareForDepositor;
+	}
+
+    function    withdraw(uint256 _shareToRedeem, address _recipient) public returns (uint256) {
+        require(shares[msg.sender] > 0, "No shares");
+
+        uint256 share = shares[msg.sender];
+        if (share > _shareToRedeem) {
+            share = _shareToRedeem;
+        }
+        uint256 redeemedValue = currentVault.withdraw(share, _recipient);
+
+        shares[msg.sender] -= share;
+        totalShares -= share;
+        return redeemedValue;
+    }
+
+    function    estimateMigration(address _token, Bowswap.Swap[] calldata _instructions) public view returns (uint256) {
+        return bowswap.estimate_out(
+            address(currentVault),
+            yRegistry.latestVault(_token),
+            metaVaultTotalShare(),
+            _instructions,
+            30
+        );
+    }
+    function    swap(address _token, uint256 _minAmountOut, Bowswap.Swap[] calldata _instructions) public {
+        address toVault = yRegistry.latestVault(_token);
+        uint256 previousTotalShare = metaVaultTotalShare();
+        uint256 previousDecimals = 10 ** currentDecimals;
+
+        currentVault.approve(address(bowswap), metaVaultTotalShare());
+        bowswap.swap(address(currentVault), toVault, metaVaultTotalShare(), _minAmountOut, _instructions, 30, 1);
+        currentVault = Vault(toVault);
+        currentDecimals = currentVault.decimals();
+
+        uint256 newTotalShare = metaVaultTotalShare();
+        uint256 newDecimals = (10 ** currentDecimals);
+        uint256 evolution = 0;
+        if (newDecimals < previousDecimals) {
+            uint256 previousScaledTotalShares = previousTotalShare * newDecimals / previousDecimals;
+            evolution = newTotalShare * previousDecimals / previousScaledTotalShares;
+        } else {
+            evolution = newTotalShare * previousDecimals / previousTotalShare;
+        }
+        power = power * evolution / 1e18;
+    }
+}
---
+++ scripts/run_eur.py
@@ -0,0 +1,136 @@
+import pytest
+from brownie import MetaVault, VaultSwapper, Contract, Wei, accounts, chain, convert
+
+PATH_EURS_TO_EURT = [[1, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 1, 0], [0, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 1, 0]]
+PATH_EURS_TO_IBEUR = [[1, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 1, 0], [0, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0]]
+PATH_EURS_TO_EURN = [[1, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 0, 0], [2, "0x3CFAa1596777CAD9f5004F9a0c443d912E262243", 2, 1], [0, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0]]
+PATH_EURS_TO_3EUR = [[1, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 0, 0], [0, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 2, 0]]
+
+PATH_EURT_TO_EURS = [[1, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 1, 0], [0, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 1, 0]]
+PATH_EURT_TO_IBEUR = [[1, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 1, 0], [0, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0]]
+PATH_EURT_TO_EURN = [[1, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 0, 0], [0, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0]]
+PATH_EURT_TO_3EUR = [[1, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 0, 0], [0, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 1, 0]]
+
+PATH_IBEUR_TO_EURS = [[1, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0], [0, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 1, 0]]
+PATH_IBEUR_TO_EURT = [[1, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0], [0, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 1, 0]]
+PATH_IBEUR_TO_EURN = [[1, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0], [2, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 1, 0], [0, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0]]
+PATH_IBEUR_TO_3EUR = [[1, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 0, 0], [2, "0xB37D6c07482Bc11cd28a1f11f1a6ad7b66Dec933", 0, 1], [0, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 0, 0]]
+
+PATH_EURN_TO_EURS = [[1, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0], [2, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 0, 1], [0, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 1, 0]]
+PATH_EURN_TO_EURT = [[1, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0], [0, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 0, 0]]
+PATH_EURN_TO_IBEUR = [[1, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0], [2, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 0, 1], [0, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0]]
+PATH_EURN_TO_3EUR = [[1, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0], [0, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 1, 0]]
+
+PATH_3EUR_TO_EURS = [[1, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 2, 0], [0, "0x0Ce6a5fF5217e38315f87032CF90686C96627CAA", 0, 0]]
+PATH_3EUR_TO_EURT = [[1, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 1, 0], [0, "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890", 0, 0]]
+PATH_3EUR_TO_IBEUR = [[1, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 0, 0], [2, "0x45a8CC73EC100306af64AB2CcB7B12E70EC549A8", 0, 1], [0, "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859", 1, 0]]
+PATH_3EUR_TO_EURN = [[1, "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571", 1, 0], [0, "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a", 1, 0]]
+
+
+curveEURSToken = "0x194eBd173F6cDacE046C53eACcE9B953F28411d1"
+curveEURSTokenWhale = "0x90bb609649e0451e5ad952683d64bd2d1f245840"
+
+curveEURTToken = "0xFD5dB7463a3aB53fD211b4af195c5BCCC1A03890"
+curveibEURToken = "0x19b080FE1ffA0553469D20Ca36219F17Fcf03859"
+curveEURNToken = "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a"
+curve3EURToken = "0xb9446c4Ef5EBE66268dA6700D26f96273DE3d571"
+
+deployer = accounts[0]
+depositor = accounts[1]
+another_depositor = accounts[2]
+bowswap = deployer.deploy(VaultSwapper)
+bowswap.initialize(deployer, {"from": deployer})
+metaVault = deployer.deploy(MetaVault, curveEURSToken, bowswap)
+eurs = Contract.from_explorer(curveEURSToken)
+# usdc = Contract.from_explorer(TOKEN_USDC)
+
+def simulate_harvest(token, whale, vault):
+	print('SIMULATING HARVEST WITH 100_000_000 USDC EARNED')
+	token.transfer(vault, 100000000e18, {'from': whale})
+
+def print_situation():
+	vaultPPS = float('{0:.2f}'.format(metaVault.underlyingPricePerShare().to('ether')))
+	depositorShare = float('{0:.2f}'.format(metaVault.getShares(depositor).to('ether')))
+	anotherDepositorShare = float('{0:.2f}'.format(metaVault.getShares(another_depositor).to('ether')))
+	print('--------------------------------')
+	print('TotalShare: ' + convert.to_string(metaVault.metaVaultTotalShare().to('ether')))
+	print('Underlying PPS: ' + convert.to_string(metaVault.underlyingPricePerShare().to('ether')))
+	print('Depositor Share: ' + convert.to_string(metaVault.getShares(depositor).to('ether')) + ' (' + str(round(vaultPPS * depositorShare)) + '$)')
+	print('Another Depositor Share: ' + convert.to_string(metaVault.getShares(another_depositor).to('ether')) + ' (' + str(round(vaultPPS * anotherDepositorShare)) + '$)')
+	print('--------------------------------')
+
+def init_metavault():
+	assert metaVault.metaVaultTotalShare() == 0
+	assert metaVault.getShares(depositor) == 0
+
+	# Take some eurs from the whales to work with
+	eurs.transfer(depositor, 1000e18, {'from': curveEURSTokenWhale})
+	assert eurs.balanceOf(depositor) >= 1000e18
+	eurs.transfer(another_depositor, 500e18, {'from': curveEURSTokenWhale})
+	assert eurs.balanceOf(another_depositor) >= 500e18
+
+	# Deposit some eurs from depositor
+	eurs.approve(metaVault.address, 1000e18, {'from': depositor})
+	assert eurs.allowance(depositor, metaVault.address) == 1000e18
+	metaVault.deposit(1000e18, depositor, {'from': depositor})
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(depositor), 1000e18, abs=1e18)
+
+	print_situation()
+
+	# Deposit some eurs from another_depositor
+	eurs.approve(metaVault.address, 500e18, {'from': another_depositor})
+	assert eurs.allowance(another_depositor, metaVault.address) == 500e18
+	metaVault.deposit(500e18, another_depositor, {'from': another_depositor})
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 1500e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(depositor), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(another_depositor), 500e18, abs=1e18)
+
+	# Withdraw some eurs from depositor
+	metaVault.withdraw(500e18, depositor, {'from': depositor})
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(depositor), 500e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(another_depositor), 500e18, abs=1e18)
+	assert pytest.approx(eurs.balanceOf(depositor), 500e18, abs=1e18)
+
+	# Withdraw some eurs from another_depositor
+	metaVault.withdraw(500e18, another_depositor, {'from': another_depositor})
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 500e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(depositor), 500e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(another_depositor), 0, abs=1e18)
+	assert pytest.approx(eurs.balanceOf(another_depositor), 500, abs=1e18)
+
+	# Re-deposit some eurs from depositor and another_depositor
+	eurs.approve(metaVault.address, 500e18, {'from': depositor})
+	assert eurs.allowance(depositor, metaVault.address) == 500e18
+	metaVault.deposit(500e18, depositor, {'from': depositor})
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(depositor), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(another_depositor), 0, abs=1e18)
+	assert pytest.approx(eurs.balanceOf(depositor), 0, abs=1e18)
+	eurs.approve(metaVault.address, 500e18, {'from': another_depositor})
+	assert eurs.allowance(another_depositor, metaVault.address) == 500e18
+	metaVault.deposit(500e18, another_depositor, {'from': another_depositor})
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 1500e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(depositor), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(another_depositor), 500e18, abs=1e18)
+	assert pytest.approx(eurs.balanceOf(another_depositor), 0, abs=1e18)
+	print_situation()
+
+def migrate(to, path):
+	estimateOut = metaVault.estimateMigration(to, path)
+	assert pytest.approx(estimateOut, 1000e18, abs=10e18)
+	metaVault.swap(to, estimateOut * 0.1, path, {'from': deployer})
+	assert pytest.approx(metaVault.power(), 1e18, abs=1e17)
+	assert pytest.approx(metaVault.metaVaultTotalShare(), 1000e6, abs=1e6)
+	assert pytest.approx(metaVault.getShares(depositor), 1000e18, abs=1e18)
+	assert pytest.approx(metaVault.getShares(another_depositor), 500e18, abs=1e18)
+	print_situation()
+
+def main():
+	init_metavault()
+	migrate(to = curveEURTToken, path = PATH_EURS_TO_EURT)
+	migrate(to = curveibEURToken, path = PATH_EURT_TO_IBEUR)
+	migrate(to = curveEURNToken, path = PATH_IBEUR_TO_EURN)
+	migrate(to = curve3EURToken, path = PATH_EURN_TO_3EUR)
+	migrate(to = curveEURSToken, path = PATH_3EUR_TO_EURS)
